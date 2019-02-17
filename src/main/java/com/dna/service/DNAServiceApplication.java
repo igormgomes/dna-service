6c5a3ee2f69299dcfce6e0e22d484ed43b8e64c3
igormgomes <Aranha1234>
2019-02-16 23:32:55 -0200

    first commit

---
+++ src/main/java/com/dna/service/DNAServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dna.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class DNAServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DNAServiceApplication.class, args);
+	}
+}
