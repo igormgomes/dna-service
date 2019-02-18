60e38801c651145888ba0d8a6e6a135461e10e7d
igormgomes <Aranha1234>
2019-02-17 21:03:52 -0300

    change pattern

--- src/main/java/com/dna/service/component/impl/SimianPattern.java
+++ src/main/java/com/dna/service/component/impl/SimianPattern.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 @Component
 public class SimianPattern {
 
-    private final String SIMIO_PATTERN = "[A]{4}|[C]{4}|[G]{4}|[T]{4}";
+    private final String SIMIO_PATTERN = "[A]{4}|[T]{4}|[C]{4}|[G]{4}";
 
     public boolean isSimio(String dna){
         Pattern pattern = Pattern.compile(SIMIO_PATTERN);
